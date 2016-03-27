2f5a9f82727bd652c28eb0c230190900edddc269
diego <diegolee7@gmail.com>
2016-03-27 19:38:26 -0300

    criada classe GenerateSignature

--- BitCoin/BitCoinPeer.java
+++ BitCoin/BitCoinPeer.java
@@ -5,7 +5,8 @@ import javax.crypto.*;
 import java.util.Arrays;
 
 public class BitCoinPeer {
-    public static void main(String args[]) throws NoSuchAlgorithmException {
+    
+    public void start() {
         // args give message contents and destination multicast group (e.g.
         // "228.5.6.7")
         MulticastSocket s = null;
@@ -15,37 +16,22 @@ public class BitCoinPeer {
             s.joinGroup(group);
             String string = "Hello";
             byte[] m = string.getBytes();
-            DatagramPacket messageOut =
-                new DatagramPacket(m, m.length, group, 6789);
+            DatagramPacket messageOut = new DatagramPacket(m, m.length, group, 6789);
             s.send(messageOut);
 
-            // Generate a 1024-bit Digital Signature Algorithm (DSA) key pair
-  
-            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("DSA");
-            keyGen.initialize(1024);
-            KeyPair keypair = keyGen.genKeyPair();
-            PrivateKey privateKey = keypair.getPrivate();
-            System.out.println(Arrays.toString(privateKey.getEncoded()));
-            PublicKey publicKey = keypair.getPublic();
-            System.out.println("PUBLICAAAAAAAAA");
-            System.out.println(Arrays.toString(publicKey.getEncoded()));
-            
             byte[] buffer = new byte[1000];
             for (int i = 0; i < 4; i++) { // get messages from others in group
-                DatagramPacket messageIn =
-                    new DatagramPacket(buffer, buffer.length);
+                DatagramPacket messageIn = new DatagramPacket(buffer, buffer.length);
                 s.receive(messageIn);
-                System.out.println("Received:" +
-                                   new String(messageIn.getData()));
+                System.out.println("Received:" + new String(messageIn.getData()));
             }
             s.leaveGroup(group);
-        } catch (SocketException e) {
-            System.out.println("Socket: " + e.getMessage());
-        } catch (IOException e) {
-            System.out.println("IO: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Exception: " + e.getMessage());
         } finally {
             if (s != null)
                 s.close();
         }
     }
+    
 }
---
+++ BitCoin/MainBitCoin.java
@@ -0,0 +1,7 @@
+
+public class MainBitCoin {
+    public static void main(String args[]) {
+	BitCoinPeer peer = new BitCoinPeer();
+	peer.start();
+    }
+}
