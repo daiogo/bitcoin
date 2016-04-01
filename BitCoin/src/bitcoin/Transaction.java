77ada0c5bb828004d970707c237b98aff68bf9f5
diego <diegolee7@gmail.com>
2016-04-01 01:10:43 -0300

    new class Transaction added

---
+++ BitCoin/src/bitcoin/Transaction.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bitcoin;
+
+/**
+ *
+ * @author diego
+ */
+public class Transaction {
+    private String sellerUsername;
+    private String buyerUsername;
+    private int coins;
+}
